42c6758078ec55d577ee06db3f00872244717956
Lamiaa21-3 <lamiaagamal213@gmail.com>
2023-09-29 23:41:39 +0200

    first commit

---
+++ android/app/src/main/kotlin/com/example/empolyee_info/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.empolyee_info
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/screens/home_page.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class HomePageScreens extends StatelessWidget {
+  const HomePageScreens({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Empolyees'),
+      ),
+      body: Column(
+        children: [
+
+        ],
+      ),
+    );
+  }
+}
